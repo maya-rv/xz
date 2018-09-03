c43c21b893ae73283b002c52d2d1aa3a59c7a36c
Maya Rashish <maya.rashish@runtimeverification.com>
2018-09-03 16:24:49 +0300

    Test with RV toolkit

--- rv.sh
+++ rv.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+apt install -y automake autoconf libtool make autopoint
+
 autoreconf -fiv
 env CC=rvpc CXX=rvpc++ LD=rvpld ./configure
 make -j`nproc`
 
-env RVP_ANALYSIS_ARGS="--output=json" RVP_REPORT_FILE="`pwd`/my_errors.json" make check
+env RVP_ANALYSIS_ARGS="--output=json" RVP_REPORT_FILE="$PWD/my_errors.json" make check
+
+report_path="$PWD/report"
+touch $RVP_REPORT_FILE && rv-html-report $RVP_REPORT_FILE -o $report_path
+
+rv-upload-report $report_path
