8593d24c693c86d4a7f54c9a68946d156cf217c3
Maya Rashish <maya.rashish@runtimeverification.com>
2018-09-07 17:34:07 +0300

    timeout

--- rv.sh
+++ rv.sh
@@ -6,7 +6,9 @@ autoreconf -fiv
 env CC=rvpc CXX=rvpc++ LD=rvpld ./configure
 make -j`nproc`
 
-env RVP_ANALYSIS_ARGS="--output=json" RVP_REPORT_FILE="$PWD/my_errors.json" make check
+export RVP_ANALYSIS_ARGS="--output=json"
+export RVP_REPORT_FILE="$PWD/my_errors.json"
+timeout 3m make check
 
 report_path="$PWD/report"
 touch $RVP_REPORT_FILE && rv-html-report $RVP_REPORT_FILE -o $report_path
